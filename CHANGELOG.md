d6ad89c8633644b89bae558ac628df9ce63c31b2
Safar Umarov <safarumarov711@gmail.com>
2021-10-07 00:35:01 +0700

    Installed Backpack\NewsCRUD

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,19 +5,23 @@
 ### Changed:
 ### Fixed:
 
-[1.4.0]
+##[1.5.0]
+### Added
+- Installed Backpack\NewsCrud
+
+##[1.4.0]
 ### Added
 - Installed Backpack\MenuCRUD
 
-[1.3.0]
+##[1.3.0]
 ### Added
 - Installed BackPack\PageManager
 
-[1.2.0]
+##[1.2.0]
 ### Added
 - Installed Backpack\PermissionManager
 
-[1.1.0]
+##[1.1.0]
 ### Added
 - Installed backpack
 
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -11,3 +11,11 @@
 
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-file-o'></i> <span>Pages</span></a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('menu-item') }}'><i class='nav-icon la la-list'></i> <span>Menu</span></a></li>
+<li class="nav-item nav-dropdown">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-newspaper-o"></i>News</a>
+    <ul class="nav-dropdown-items">
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('article') }}"><i class="nav-icon la la-newspaper-o"></i> Articles</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('category') }}"><i class="nav-icon la la-list"></i> Categories</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('tag') }}"><i class="nav-icon la la-tag"></i> Tags</a></li>
+    </ul>
+</li>
